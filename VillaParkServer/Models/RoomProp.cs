ed03b356f5a55bac57c6276cabf2559dd5a53668
syariefa <syareeefa@gmail.com>
2021-06-26 23:02:33 +0700

    Dropdown and loops

---
+++ VillaParkServer/Models/RoomProp.cs
@@ -0,0 +1,9 @@
+namespace VillaParkServer.Models
+{
+    public class RoomProp
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Value { get; set; }
+    }
+}
